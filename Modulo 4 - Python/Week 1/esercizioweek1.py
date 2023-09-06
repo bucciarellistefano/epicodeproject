fa8891e6e0f64cdd3d3cef4b408fe7f9ac239d70
bucciarellistefano <130074848+bucciarellistefano@users.noreply.github.com>
2023-09-06 19:15:11 +0200

    Add files via upload

---
+++ Modulo 4 - Python/Week 1/esercizioweek1.py
@@ -0,0 +1,13 @@
+studenti = 25
+print(studenti)
+
+studenti2 = 3
+
+somma_studenti = studenti + studenti2
+print(somma_studenti)
+
+lista_numeri = [0, 1, 2, 3, 4, 5]
+print("La lista dei numeri è:", lista_numeri)
+
+titoli_film = ["Il Re Leone", "Captain Hook", "Toy Story"]
+print("Alcuni dei miei film preferiti sono:", titoli_film)
